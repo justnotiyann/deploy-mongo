28298809dbd17d3efa53ee501a41f070b56cacc3
justnotiyann <muhammadfitrian0712@gmail.com>
2022-10-30 00:41:04 +0700

    add gulp method

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,27 @@ const gulp = require("gulp");
 const gulpUglify = require("gulp-uglify");
 const clean = require("gulp-clean");
 
+gulp.task("minify-js-index", async function () {
+     gulp.src(["index.js"]).pipe(gulpUglify()).pipe(gulp.dest("dist/"));
+});
+
 gulp.task("minify-js-routes", async function () {
-     gulp.src(["routes/*.js"])
+     gulp.src(["src/routes/*.js"])
+          .pipe(gulpUglify())
+          .pipe(gulp.dest("dist/routes"));
+});
+gulp.task("minify-js-routes", async function () {
+     gulp.src(["src/routes/*.js"])
           .pipe(gulpUglify())
           .pipe(gulp.dest("dist/routes"));
 });
 gulp.task("minify-js-models", async function () {
-     gulp.src(["models/*.js"])
+     gulp.src(["src/models/*.js"])
           .pipe(gulpUglify())
           .pipe(gulp.dest("dist/models"));
 });
 gulp.task("minify-js-config", async function () {
-     gulp.src(["config/*.js"])
+     gulp.src(["src/config/*.js"])
           .pipe(gulpUglify())
           .pipe(gulp.dest("dist/config"));
 });
@@ -31,6 +40,7 @@ gulp.task(
      "build",
      gulp.series(
           "clean",
+          "minify-js-index",
           "minify-js-routes",
           "minify-js-models",
           "minify-js-config"
